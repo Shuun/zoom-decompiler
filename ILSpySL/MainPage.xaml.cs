bff1ed2c7e25177e5b442da9ab37b93542a02efa
mihailik <mihailik@gmail.com>
2011-04-28 09:13:48 +0100

    Added the initial layout: TreeView, RichTextBox and Open button.
    Mono.Cecil works, DecompilerContext is being created. Now need to figure out how to kick it off and do any useful 'AST to text' conversion.

--- ILSpySL/MainPage.xaml.cs
+++ ILSpySL/MainPage.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using ICSharpCode.Decompiler;
 
 namespace ILSpySL
 {
@@ -17,6 +18,20 @@ namespace ILSpySL
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() != true)
+                return;
+
+            Mono.Cecil.ModuleDefinition module;
+            using (var stream = openFileDialog.File.OpenRead())
+            {
+                module = Mono.Cecil.ModuleDefinition.ReadModule(stream);
+            }
+            DecompilerContext decompiler = new DecompilerContext(module);
         }
     }
 }
