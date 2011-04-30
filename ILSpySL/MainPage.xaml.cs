2ab3bfd39a3abc675f6f6baf065b5f82da5b0d7c
mihailik <mihailik@gmail.com>
2011-04-30 08:54:30 +0100

    Update to the code using Decompiler. Now it does more of the actual decompiling.

--- ILSpySL/MainPage.xaml.cs
+++ ILSpySL/MainPage.xaml.cs
@@ -10,11 +10,46 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using ICSharpCode.Decompiler;
+using ICSharpCode.Decompiler.Ast;
+using Mono.Cecil;
+using System.IO;
 
 namespace ILSpySL
 {
     public partial class MainPage : UserControl
     {
+        private sealed class Resolver : IAssemblyResolver
+        {
+            readonly Func<AssemblyNameReference, ReaderParameters, AssemblyDefinition> resolve;
+
+            public Resolver(Func<AssemblyNameReference, ReaderParameters, AssemblyDefinition> resolve)
+            {
+                this.resolve = resolve;
+            }
+
+            public AssemblyDefinition Resolve(AssemblyNameReference name)
+            {
+                return Resolve(name, null);
+            }
+
+            public AssemblyDefinition Resolve(AssemblyNameReference name, ReaderParameters parameters)
+            {
+                return this.resolve(name, parameters);
+            }
+
+            public AssemblyDefinition Resolve(string fullName)
+            {
+                return Resolve(fullName, null);
+            }
+
+            public AssemblyDefinition Resolve(string fullName, ReaderParameters parameters)
+            {
+                return Resolve(AssemblyNameReference.Parse(fullName), parameters);
+            }
+        }
+
+        readonly List<AssemblyDefinition> assembies = new List<AssemblyDefinition>();
+
         public MainPage()
         {
             InitializeComponent();
@@ -26,12 +61,82 @@ namespace ILSpySL
             if (openFileDialog.ShowDialog() != true)
                 return;
 
-            Mono.Cecil.ModuleDefinition module;
+            var buf = new MemoryStream();
             using (var stream = openFileDialog.File.OpenRead())
             {
-                module = Mono.Cecil.ModuleDefinition.ReadModule(stream);
+                while(true)
+                {
+                    int b = stream.ReadByte();
+                    if(b<0)
+                        break;
+                    else
+                        buf.WriteByte((byte)b);
+                }
             }
-            DecompilerContext decompiler = new DecompilerContext(module);
+
+            buf.Position = 0;
+
+            openButton.IsEnabled = false;
+
+            System.Threading.ThreadPool.QueueUserWorkItem(delegate
+            {
+                try
+                {
+                    var para = new Mono.Cecil.ReaderParameters(Mono.Cecil.ReadingMode.Immediate);
+                    Mono.Cecil.AssemblyDefinition asm;
+                        para.AssemblyResolver = new Resolver(
+                            (asmName, _p) =>
+                                assembies.FirstOrDefault(
+                                    loaded =>
+                                        string.Equals(
+                                        loaded.FullName,
+                                        asmName.FullName,
+                                        StringComparison.OrdinalIgnoreCase)));
+
+                    asm = Mono.Cecil.AssemblyDefinition.ReadAssembly(buf, para);
+
+                    this.assembies.Add(asm);
+
+                    this.Dispatcher.BeginInvoke(delegate
+                    {
+                        this.treeView1.Items.Add(new TreeViewItem
+                        {
+                            Header = asm.Name.Name + " v" + asm.Name.Version
+                        });
+                    });
+
+                    var ctx = new DecompilerContext(asm.MainModule);
+                    var astBui = new AstBuilder(ctx);
+                    astBui.AddAssembly(asm);
+                    astBui.RunTransformations();
+                    var outp = new PlainTextOutput();
+                    astBui.GenerateCode(outp);
+
+                    this.Dispatcher.BeginInvoke(delegate
+                    {
+                        openButton.IsEnabled = true;
+                        var rn = new Run { Text = outp.ToString() };
+                        var pa = new Paragraph();
+                        pa.Inlines.Add(rn);
+
+                        codeTextBox.Blocks.Clear();
+                        codeTextBox.Blocks.Add(pa);
+                    });
+                }
+                catch(Exception error)
+                {
+                    this.Dispatcher.BeginInvoke(delegate
+                    {
+                        openButton.IsEnabled = true;
+                        var rn = new Run { Text = error.ToString() };
+                        var pa = new Paragraph();
+                        pa.Inlines.Add(rn);
+
+                        codeTextBox.Blocks.Clear();
+                        codeTextBox.Blocks.Add(pa);
+                    });
+                }
+            });
         }
     }
 }
