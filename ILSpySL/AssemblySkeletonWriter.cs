e1cbe9f3b7a87214499c2222ff5f82377a1e8b64
mihailik <mihailik@gmail.com>
2011-05-03 09:12:02 +0100

    Creating the classes for packing/unpacking platform's core assembly 'skeleton' information -- so ILSpy can correctly decompile assemblies compiled for the platforms others than the current (Silverlight 4 in this case).

---
+++ ILSpySL/AssemblySkeletonWriter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+using Mono.Cecil;
+
+namespace ILSpySL
+{
+    public sealed class AssemblySkeletonWriter
+    {
+        private sealed class ChunkWriter
+        {
+            internal void WriteAssemblyName(AssemblyNameDefinition assemblyNameDefinition)
+            {
+                throw new NotImplementedException();
+            }
+
+            internal void WriteAttributes(IEnumerable<CustomAttribute> collection)
+            {
+                throw new NotImplementedException();
+            }
+
+            internal void WriteChunk(ChunkWriter typeWriter)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        readonly List<ChunkWriter> assemblyChunkList = new List<ChunkWriter>();
+
+        private AssemblySkeletonWriter()
+        {
+        }
+
+        public static void WriteSkeletons(IEnumerable<AssemblyDefinition> assemblies, Stream outputStream)
+        {
+            if (assemblies == null)
+                throw new ArgumentNullException("assemblies");
+
+            var writer = new AssemblySkeletonWriter();
+            
+            foreach (var a in assemblies)
+            {
+                writer.AddAssembly(a);
+            }
+
+            writer.WriteToStream(outputStream);
+        }
+
+        void AddAssembly(AssemblyDefinition asm)
+        {
+            var assemblyChunk = new ChunkWriter();
+
+            assemblyChunk.WriteAssemblyName(asm.Name);
+            assemblyChunk.WriteAttributes(asm.CustomAttributes);
+
+            foreach (var t in asm.MainModule.Types)
+            {
+                var typeWriter = new ChunkWriter();
+
+                AddType(t, typeWriter);
+
+                assemblyChunk.WriteChunk(typeWriter);
+            }
+
+            assemblyChunkList.Add(assemblyChunk);
+        }
+
+        private void AddType(TypeDefinition t, ChunkWriter typeWriter)
+        {
+            throw new NotImplementedException();
+        }
+
+        void WriteToStream(Stream outputStream)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
