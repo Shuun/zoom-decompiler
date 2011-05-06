907d2f1244cd1b364c98a6eca0cfdc8bc4b913ea
mihailik <mihailik@gmail.com>
2011-05-06 08:58:40 +0100

    Let the pretend ReaderWriterLockSlim not throw exceptions, it will just silently not lock.

--- ICSharpCode.Decompiler/Compatibility/ReaderWriterLockSlim.cs
+++ ICSharpCode.Decompiler/Compatibility/ReaderWriterLockSlim.cs
@@ -6,22 +6,18 @@ namespace ICSharpCode
     {
         public void EnterWriteLock()
         {
-            throw new NotImplementedException();
         }
 
         public void ExitWriteLock()
         {
-            throw new NotImplementedException();
         }
 
         public void EnterReadLock()
         {
-            throw new NotImplementedException();
         }
 
         public void ExitReadLock()
         {
-            throw new NotImplementedException();
         }
     }
 }
