9742663e1b5008821b04e744a40672d26344fba0
mihailik <mihailik@gmail.com>
2011-05-06 09:31:22 +0100

    Adding layout side into things.

--- ILSpySL/Model/AssemblyNamespaceIsland.cs
+++ ILSpySL/Model/AssemblyNamespaceIsland.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace ILSpySL.Model
 {
@@ -13,21 +14,31 @@ namespace ILSpySL.Model
         readonly AssemblyNamespaceIsland m_ParentNamespace;
         readonly string m_Name;
         readonly ReadOnlyCollection<TypeIsland> m_Types;
+        readonly Size m_DesiredSize;
+        readonly Point m_RelativePosition;
 
         public AssemblyNamespaceIsland(
             AssemblyIsland assembly, AssemblyNamespaceIsland parentNamespace, string name,
-            Func<AssemblyNamespaceIsland, IEnumerable<TypeIsland>> types)
+            Func<AssemblyNamespaceIsland, IEnumerable<TypeIsland>> getTypes,
+            Func<AssemblyNamespaceIsland, Size> getDesiredSize,
+            Func<AssemblyNamespaceIsland, Point> getRelativePosition)
         {
             this.m_Assembly = assembly;
             this.m_ParentNamespace = parentNamespace;
             this.m_Name = name;
 
-            this.m_Types = types(this).ToReadOnlyCollectionOrNull();
+            this.m_Types = getTypes(this).ToReadOnlyCollectionOrNull();
+
+            this.m_DesiredSize = getDesiredSize(this);
+
+            this.m_RelativePosition = getRelativePosition(this);
         }
 
         public AssemblyIsland Assembly { get { return this.m_Assembly; } }
         public AssemblyNamespaceIsland ParentNamespace { get { return this.m_ParentNamespace; } }
         public string Name { get { return this.m_Name; } }
-        public ReadOnlyCollection<TypeIsland> Types { get { return this.m_Types ?? Empty.ReadOnlyCollection<TypeIsland>(); }}
+        public ReadOnlyCollection<TypeIsland> Types { get { return this.m_Types ?? Empty.ReadOnlyCollection<TypeIsland>(); } }
+        public Size DesiredSize { get { return this.m_DesiredSize; } }
+        public Point RelativePosition { get { return this.m_RelativePosition; } }
     }
 }
--- ILSpySL/Model/TypeIsland.cs
+++ ILSpySL/Model/TypeIsland.cs
@@ -1,21 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace ILSpySL.Model
 {
+    using ICSharpCode.Decompiler;
+
     public sealed class TypeIsland
     {
         readonly AssemblyNamespaceIsland m_Namespace;
         readonly string m_Name;
+        readonly ReadOnlyCollection<CodeLine> m_Lines;
+        readonly Point m_RelativePosition;
 
-        public TypeIsland(AssemblyNamespaceIsland @namespace, string name)
+        public TypeIsland(
+            AssemblyNamespaceIsland @namespace, string name, IEnumerable<CodeLine> lines,
+            Func<TypeIsland, Point> getRelativePosition)
         {
             this.m_Namespace = @namespace;
             this.m_Name = name;
+            this.m_Lines = lines.ToReadOnlyCollectionOrNull();
+            this.m_RelativePosition = getRelativePosition(this);
         }
 
         public AssemblyNamespaceIsland Namespace { get { return this.m_Namespace; } }
         public string Name { get { return this.m_Name; } }
+        public ReadOnlyCollection<CodeLine> Lines { get { return this.m_Lines ?? Empty.ReadOnlyCollection<CodeLine>(); } }
+        public Point RelativePosition { get { return this.m_RelativePosition; } }
     }
 }
