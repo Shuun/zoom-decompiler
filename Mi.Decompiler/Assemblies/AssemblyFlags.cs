92397a43b29f4c8c9325a7e428a2371b854929aa
mihailik <mihailik@gmail.com>
2011-05-09 09:22:51 +0100

    No sense inheriting AssemblyAttributes enum from uint. Defaulting to int.

--- Mi.Decompiler/Assemblies/AssemblyFlags.cs
+++ Mi.Decompiler/Assemblies/AssemblyFlags.cs
@@ -28,14 +28,15 @@
 
 using System;
 
-namespace Mi.Assemblies {
-
-	[Flags]
-	public enum AssemblyAttributes : uint {
-		PublicKey					 	= 0x0001,
-		SideBySideCompatible			= 0x0000,
-		Retargetable					= 0x0100,
-		DisableJITCompileOptimizer		= 0x4000,
-		EnableJITCompileTracking		= 0x8000,
-	}
+namespace Mi.Assemblies
+{
+    [Flags]
+    public enum AssemblyAttributes
+    {
+        PublicKey = 0x0001,
+        SideBySideCompatible = 0x0000,
+        Retargetable = 0x0100,
+        DisableJITCompileOptimizer = 0x4000,
+        EnableJITCompileTracking = 0x8000,
+    }
 }
