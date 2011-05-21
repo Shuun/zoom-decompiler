fe08433f15529182603c4ac3021d7db3777b87b3
Daniel Grunwald <daniel@danielgrunwald.de>
2011-05-21 21:57:53 +0200

    Add support for type forwarding declarations ([TypeForwardedToAttribute]). Closes #190.

---
+++ ICSharpCode.Decompiler/Tests/ExceptionHandling.cs
@@ -0,0 +1,93 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under MIT X11 license (for details please see \doc\license.txt)
+
+using System;
+using System.Threading;
+
+public class ExceptionHandling
+{
+	public void MethodEndingWithEndFinally()
+	{
+		try
+		{
+			throw null;
+		}
+		finally
+		{
+			Console.WriteLine();
+		}
+	}
+	
+	public void MethodEndingWithRethrow()
+	{
+		try
+		{
+			throw null;
+		}
+		catch
+		{
+			throw;
+		}
+	}
+	
+	public void TryCatchFinally()
+	{
+		try
+		{
+			Console.WriteLine("Try");
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex.Message);
+		}
+		finally
+		{
+			Console.WriteLine("Finally");
+		}
+	}
+	
+	public void TryCatchMultipleHandlers()
+	{
+		try
+		{
+			Console.WriteLine("Try");
+		}
+		catch (InvalidOperationException ex)
+		{
+			Console.WriteLine(ex.Message);
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex.Message);
+		}
+		catch
+		{
+			Console.WriteLine("other");
+		}
+	}
+	
+	public void NoUsingStatementBecauseTheVariableIsAssignedTo()
+	{
+		CancellationTokenSource cancellationTokenSource = null;
+		try
+		{
+			cancellationTokenSource = new CancellationTokenSource();
+		}
+		finally
+		{
+			if (cancellationTokenSource != null)
+			{
+				cancellationTokenSource.Dispose();
+			}
+		}
+	}
+	
+	public void UsingStatementThatChangesTheVariable()
+	{
+		CancellationTokenSource cancellationTokenSource = null;
+		using (cancellationTokenSource)
+		{
+			cancellationTokenSource = new CancellationTokenSource();
+		}
+	}
+}
