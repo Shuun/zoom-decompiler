6ea45cad93b9c9da9bdabd159fe958b03956dcf1
mihailik <mihailik@gmail.com>
2011-05-23 19:00:52 +0100

    Everyting compiles again (some problems with tests for CustomAttributes).

--- Mi.Decompiler.Tests/Helpers/RemoveCompilerAttribute.cs
+++ Mi.Decompiler.Tests/Helpers/RemoveCompilerAttribute.cs
@@ -5,12 +5,13 @@ using System.Text;
 
 using Mi.Decompiler.Ast.Transforms;
 using Mi.CSharp;
+using Mi.CSharp.Ast;
 
 namespace Mi.Decompiler.Tests.Helpers
 {
 	class RemoveCompilerAttribute : DepthFirstAstVisitor<object, object>, IAstTransform
 	{
-		public override object VisitAttribute(CSharp.Attribute attribute, object data)
+		public override object VisitAttribute(CSharp.Ast.Attribute attribute, object data)
 		{
 			var section = (AttributeSection)attribute.Parent;
 			SimpleType type = attribute.Type as SimpleType;
