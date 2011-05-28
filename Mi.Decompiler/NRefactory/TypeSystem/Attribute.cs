dc4cf18c80d789a59a339465ad9eb4efaa4cec4f
mihailik <mihailik@gmail.com>
2011-05-28 11:53:17 +0100

    Further disabling Attribute.

--- Mi.Decompiler/NRefactory/TypeSystem/Attribute.cs
+++ Mi.Decompiler/NRefactory/TypeSystem/Attribute.cs
@@ -15,124 +15,81 @@ namespace Mi.NRefactory.TypeSystem
 	/// </summary>
 	public sealed class Attribute : AbstractFreezable, IAttribute, ISupportsInterning
 	{
-		ITypeReference attributeType;
-		readonly ITypeReference[] constructorParameterTypes;
-		DomRegion region;
-		IList<IConstantValue> positionalArguments;
-		IList<KeyValuePair<string, IConstantValue>> namedArguments;
-		
-		protected override void FreezeInternal()
-		{
-			positionalArguments = FreezeList(positionalArguments);
-			
-			if (namedArguments == null || namedArguments.Count == 0) {
-                namedArguments = Empty.ReadOnlyCollection<KeyValuePair<string, IConstantValue>>();
-			} else {
-				namedArguments = Array.AsReadOnly(namedArguments.ToArray());
-				foreach (var pair in namedArguments) {
-					pair.Value.Freeze();
-				}
-			}
-			
-			base.FreezeInternal();
-		}
-		
 		public Attribute(ITypeReference attributeType, IEnumerable<ITypeReference> constructorParameterTypes)
 		{
             throw new NotSupportedException();
-
-			if (attributeType == null)
-				throw new ArgumentNullException("attributeType");
-			this.attributeType = attributeType;
-			this.constructorParameterTypes = constructorParameterTypes != null ? constructorParameterTypes.ToArray() : null;
 		}
 		
 		public ITypeReference AttributeType {
-			get { return attributeType; }
+            get { throw new NotSupportedException(); }
 		}
 		
 		public ReadOnlyCollection<ITypeReference> ConstructorParameterTypes {
-			get { return Array.AsReadOnly(constructorParameterTypes); }
+            get { throw new NotSupportedException(); }
 		}
 		
 		public DomRegion Region {
-			get { return region; }
+            get { throw new NotSupportedException(); }
 			set {
-				CheckBeforeMutation();
-				region = value;
+                throw new NotSupportedException();
 			}
 		}
 		
 		public IList<IConstantValue> PositionalArguments {
 			get {
-				if (positionalArguments == null)
-					positionalArguments = new List<IConstantValue>();
-				return positionalArguments;
+                throw new NotSupportedException();
 			}
-		}
-		
-		IList<IConstantValue> IAttribute.GetPositionalArguments(ITypeResolveContext context)
-		{
-			return this.PositionalArguments;
 		}
 		
 		public IList<KeyValuePair<string, IConstantValue>> NamedArguments {
 			get {
-				if (namedArguments == null)
-					namedArguments = new List<KeyValuePair<string, IConstantValue>>();
-				return namedArguments;
+                throw new NotSupportedException();
 			}
 		}
-		
-		IList<KeyValuePair<string, IConstantValue>> IAttribute.GetNamedArguments(ITypeResolveContext context)
-		{
-			return this.NamedArguments;
-		}
-		
-		public override string ToString()
-		{
-			StringBuilder b = new StringBuilder();
-			b.Append('[');
-			b.Append(attributeType.ToString());
-			if (this.PositionalArguments.Count + this.NamedArguments.Count > 0) {
-				b.Append('(');
-				bool first = true;
-				foreach (var element in this.PositionalArguments) {
-					if (first) first = false; else b.Append(", ");
-					b.Append(element.ToString());
-				}
-				foreach (var pair in this.NamedArguments) {
-					if (first) first = false; else b.Append(", ");
-					b.Append(pair.Key);
-					b.Append('=');
-					b.Append(pair.Value.ToString());
-				}
-				b.Append(')');
-			}
-			b.Append(']');
-			return b.ToString();
-		}
-		
-		void ISupportsInterning.PrepareForInterning(IInterningProvider provider)
-		{
-			attributeType = provider.Intern(attributeType);
-			if (constructorParameterTypes != null) {
-				for (int i = 0; i < constructorParameterTypes.Length; i++) {
-					constructorParameterTypes[i] = provider.Intern(constructorParameterTypes[i]);
-				}
-			}
-			positionalArguments = provider.InternList(positionalArguments);
-		}
-		
-		int ISupportsInterning.GetHashCodeForInterning()
-		{
-			return attributeType.GetHashCode() ^ (positionalArguments != null ? positionalArguments.GetHashCode() : 0) ^ (namedArguments != null ? namedArguments.GetHashCode() : 0) ^ region.GetHashCode();
-		}
-		
-		bool ISupportsInterning.EqualsForInterning(ISupportsInterning other)
-		{
-			Attribute a = other as Attribute;
-			return a != null && attributeType == a.attributeType && positionalArguments == a.positionalArguments && namedArguments == a.namedArguments && region == a.region;
-		}
-	}
+
+        void ISupportsInterning.PrepareForInterning(IInterningProvider provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        int ISupportsInterning.GetHashCodeForInterning()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool ISupportsInterning.EqualsForInterning(ISupportsInterning other)
+        {
+            throw new NotImplementedException();
+        }
+
+        DomRegion IAttribute.Region
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        ITypeReference IAttribute.AttributeType
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        IList<IConstantValue> IAttribute.GetPositionalArguments(ITypeResolveContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        IList<KeyValuePair<string, IConstantValue>> IAttribute.GetNamedArguments(ITypeResolveContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IFreezable.IsFrozen
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        void IFreezable.Freeze()
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
