03b6433259e2b59577ad93ad9307a154bfd0df96
mihailik <mihailik@gmail.com>
2011-06-02 13:22:47 +0100

    Added a test for 'goto' statement.

---
+++ Mi.Decompiler.Tests/Goto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public static class Goto
+{
+    private static bool value;
+
+    public static void Run()
+    {
+        while (Goto.value)
+        {
+            while (Goto.value)
+            {
+                if (Goto.value)
+                {
+                    goto IL_2A;
+                }
+            }
+        }
+    IL_2A:
+        if (Goto.value)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
