81a666d897506503d631ff682084d85ef8530cb1
mihailik <mihailik@gmail.com>
2011-06-03 08:49:43 +0100

    Moved the files from Scope into Zoom folder.

---
+++ Zoom/ZoomRenderDemo/AssemblyMap.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Mi.Assemblies;
+
+namespace Mi.Zoom.ZoomRenderDemo
+{
+    public partial class AssemblyMap : UserControl
+    {
+        public AssemblyMap()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadAssembly(AssemblyDefinition asm)
+        {
+            
+        }
+    }
+}
