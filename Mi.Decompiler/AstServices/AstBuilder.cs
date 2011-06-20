a45b3c3d99f0856c97a27ae7321299883c830953
mihailik <mihailik@gmail.com>
2011-06-20 08:44:45 +0100

    Copyright #region in AstBuilder.

--- Mi.Decompiler/AstServices/AstBuilder.cs
+++ Mi.Decompiler/AstServices/AstBuilder.cs
@@ -1,3 +1,4 @@
+#region Copyright
 // Copyright (c) 2011 AlphaSierraPapa for the SharpDevelop Team
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this
@@ -15,6 +16,7 @@
 // FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
+#endregion
 
 using System;
 using System.Collections.Generic;
