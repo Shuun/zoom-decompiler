e4dcc13c484dd7856f2be0a833ed950e97f872c7
mihailik <mihailik@gmail.com>
2011-07-07 08:59:31 +0100

    Normalized the namespace of IAnnotatable.

--- Mi.Decompiler/NRefactory/TypeSystem/ClassType.cs
+++ Mi.Decompiler/NRefactory/TypeSystem/ClassType.cs
@@ -1,5 +1,7 @@
-﻿// Copyright (c) 2010 AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+﻿#region Copyright
+// Copyright (c) 2010 AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
 // This code is distributed under MIT X11 license (for details please see \doc\license.txt)
+#endregion
 
 using System;
 
