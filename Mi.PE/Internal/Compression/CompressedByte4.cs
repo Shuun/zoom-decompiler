dd34b19ab20fb17eef4317cb87fdd3251f4d181b
mihailik <mihailik@gmail.com>
2011-07-07 07:29:20 +0100

    Compression (not really good one).

---
+++ Mi.PE/Internal/Compression/CompressedByte4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Mi.PE.Internal.Compression
+{
+    public sealed class CompressedByte4 : CompressedByteChunk
+    {
+        readonly byte b1;
+        readonly byte b2;
+        readonly byte b3;
+        readonly byte b4;
+
+        public CompressedByte4(byte b1, byte b2, byte b3, byte b4, CompressedByteChunk lead)
+            : base(4, lead)
+        {
+            this.b1 = b1;
+            this.b2 = b2;
+            this.b3 = b3;
+            this.b4 = b4;
+        }
+
+        protected override void DecompressChunkTo(byte[] buffer, int offset)
+        {
+            buffer[offset - 3] = b1;
+            buffer[offset - 2] = b2;
+            buffer[offset - 1] = b3;
+            buffer[offset] = b4;
+        }
+    }
+}
---
+++ Mi.PE/Internal/Compression/CompressedByteChunk.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Mi.PE.Internal.Compression
+{
+    public abstract class CompressedByteChunk
+    {
+        public readonly int TotalSize;
+        protected readonly CompressedByteChunk Lead;
+
+        protected CompressedByteChunk(int chunkSize, CompressedByteChunk lead)
+        {
+            this.TotalSize = chunkSize + (lead == null ? 0 : lead.TotalSize);
+            this.Lead = lead;
+        }
+
+        public byte[] Decompress()
+        {
+            byte[] result = new byte[TotalSize];
+            int offset = result.Length;
+
+            var writeChunk = this;
+            do
+            {
+                offset -= writeChunk.Lead == null ? 0 : this.TotalSize - Lead.TotalSize;
+
+                writeChunk.DecompressChunkTo(result, offset);
+
+                writeChunk = writeChunk.Lead;
+
+            } while (writeChunk != null);
+
+            return result;
+        }
+
+        protected abstract void DecompressChunkTo(byte[] buffer, int offset);
+
+        public abstract CompressedByteChunk CompressNext(byte b);
+    }
+}
